a7db55350afd7bd8364881d04f4957adebe29b76
Edson Cristovam <pg2006pe@hotmail.com>
2022-10-27 23:30:21 -0300

    update print TwitterToTelegram

--- classes/TwitterToTelegram.py
+++ classes/TwitterToTelegram.py
@@ -8,10 +8,12 @@ class TwitterToTelegram:
         self.__channel_id = channel_id
         self.__url = 'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}'
 
-    def send_message(self, message):
+    def send_message(self, message, verbose=False):
         url = self.__url.format(
             token=self.__token,
             chat_id=self.__channel_id,
             message=message
         )
-        print(requests.get(url).json())
+        response = requests.get(url).json()
+        if verbose:
+            print(response)
